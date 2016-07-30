3db8fb191db5c110cdc58aa9c7bc76d6a632bafb
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-30 01:22:38 -0500

    using variables

---
+++ app.js
@@ -0,0 +1,14 @@
+/****************************************************
+*           Using Variables with Strings            *
+****************************************************/
+var name = "Lucas";
+var age = 24;
+document.write(name + " is my name and my age " + age + "<br>");
+
+var tuna = "I am a fish";
+var bacon = "baconator?";
+var ham = "hamburgler";
+document.write(
+  tuna + "<br>" +
+  bacon + "<br>" +
+  ham);
